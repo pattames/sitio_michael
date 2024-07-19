699b886627749beb73c65b259701465bf2895d79
Tamés <patriciotames@Patricios-Mac-mini.local>
2024-07-19 15:33:41 -0600

    landing page lista

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,31 +1,27 @@
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100 font-serif text-gray-700">
+    <div className="navbar bg-base-100 font-serif text-gray-700 max-w-7xl mx-auto shadow-md">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8"
               fill="none"
               viewBox="0 0 24 24"
-              stroke="currentColor"
+              className="inline-block h-8 w-8 stroke-current"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
             </svg>
           </div>
           <ul
             tabIndex={0}
             className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Principal</a>
-            </li>
             <li>
               <a>Servicios</a>
             </li>
@@ -37,12 +33,12 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
+        <a className="btn btn-ghost text-md text-red-800 tracking-wider underline underline-offset-8 ml-4 md:text-lg">
+          SERVICIOS JURÍDICOS
+        </a>
       </div>
-      <div className="navbar-center hidden md:flex">
-        <ul className="menu menu-horizontal gap-16 px-1 text-xl">
-          <li>
-            <a>Principal</a>
-          </li>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-xl xl:gap-16">
           <li>
             <a>Servicios</a>
           </li>
@@ -55,7 +51,7 @@ export default function Navbar() {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="mx-4 text-xl hidden">Contácto</a>
+        {/* <a className="mx-8 text-xl">Contácto</a> */}
       </div>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="text-center p-2 font-serif text-gray-600">
-      <h1 className="font-bold text-3xl my-10 mt-20 text-gray-700 underline">
-        Michael Solley
-      </h1>
-      <p className="text-xl my-10">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum dolorum
-        debitis praesentium non qui minus? Laudantium odio sit harum quo.
+      <div className="my-8 md:my-16">
+        <Image
+          src="/balanza3gris.png"
+          width={150}
+          height={150}
+          alt="Balanza"
+          className="mx-auto w-38 h-auto md:w-56"
+        />
+      </div>
+      <p className="text-xl max-w-3xl mx-auto shadow-lg rounded-md p-6">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et a nihil
+        delectus sapiente laudantium beatae in officiis. Soluta, dicta adipisci
+        inventore ipsum dolore molestiae dolorem.
       </p>
-      <div className="text-lg">
+      <div className="text-lg my-10">
         <p>¿No sabes qué tipo de servicio necesitas?</p>
-        <p>Contáctanos por WhatsApp</p>
+        <p>
+          <Link
+            href="https://wa.me/525586889492?text=Hola!%20Estoy%20buscando%20asesoría%20jurídica%20debido%20a%20"
+            target="_blank"
+            className="underline text-blue-700"
+          >
+            Contáctanos
+          </Link>{" "}
+          por WhatsApp
+        </p>
       </div>
     </div>
   );
